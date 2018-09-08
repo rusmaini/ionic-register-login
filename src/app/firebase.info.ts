d99778517ba7020caecbac75c9f8a871e1922e94
rusmaini <rusmaini@gmail.com>
2018-09-09 07:31:04 +0800

    Setting API Firebase
    Gunakan API Key anda sendiri..

--- src/app/firebase.info.ts
+++ src/app/firebase.info.ts
@@ -1,12 +1,12 @@
 export const FIREBASE_INFO = {
 
     
-        apiKey: "AIzaSyBfFfYjCa0aM1yc2yDRUHXGYUpEGYrFA9s",
-        authDomain: "ionicfirebase-64f75.firebaseapp.com",
-        databaseURL: "https://ionicfirebase-64f75.firebaseio.com",
-        projectId: "ionicfirebase-64f75",
-        storageBucket: "ionicfirebase-64f75.appspot.com",
-        messagingSenderId: "918242406957"
+        apiKey: "?",
+        authDomain: "ionicfirebase-?.firebaseapp.com",
+        databaseURL: "https://ionicfirebase-?.firebaseio.com",
+        projectId: "ionicfirebase-?",
+        storageBucket: "ionicfirebase-?.appspot.com",
+        messagingSenderId: "?"
 
    
 };
