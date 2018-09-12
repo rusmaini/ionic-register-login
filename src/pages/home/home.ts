38348a2278186bb21f986491b5e5f0c869641578
maini <rusmaini@gmail.com>
2018-09-12 16:48:16 +0800

    google map
    paparkan google map dalam home..

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController ,ToastController} from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { Profile } from '../../models/profile';
+//import { Profile } from '../../models/profile';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -11,6 +11,13 @@ import { Observable } from 'rxjs/Observable';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  location: {
+    latitude: number,
+    longitude: number
+  };
 
   profileData: Observable<any>
 
@@ -18,10 +25,20 @@ export class HomePage {
     private fireAuth: AngularFireAuth, 
     private fireDatabase: AngularFireDatabase,
     public navCtrl: NavController, 
-    private toast:ToastController) {
+    private toast:ToastController  
+  ) {
   }
 
-  
+  initMap() {
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 10,
+      center: {lat: 3.1390, lng: 101.6869}
+    });
+
+    //this.directionsDisplay.setMap(this.map);
+  }
+
   ionViewWillLoad(){
     this.fireAuth.authState.subscribe(data=>{
       if(data && data.email && data.uid){
@@ -30,6 +47,9 @@ export class HomePage {
           duration: 3000
         }).present();
         this.profileData = this.fireDatabase.object('profile/'+data.uid).valueChanges();
+        
+        this.initMap();
+
         console.log(this.profileData);
       }
       else{
